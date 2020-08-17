652b019e1c80e7d3e61867cb26d3b05668b6f3e2
Athora190 <jademarte7@gmail.com>
2020-08-17 13:32:55 -0400

    challenge 2

---
+++ challenge_2/part 1/script.js
@@ -0,0 +1,7 @@
+function rightFunction() {
+  document.getElementById("imright").innerHTML = "I'm Right.";
+}
+
+function otherFunction() {
+  document.getElementById("noMe").innerHTML = "No, I'm right!";
+}
