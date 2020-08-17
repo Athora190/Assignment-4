7e7af8300ba777c0d89902d3910789b9b3eb8964
Athora190 <jademarte7@gmail.com>
2020-08-17 14:08:00 -0400

    almost done

--- challenge_2/part_1/script.js
+++ challenge_2/part_1/script.js
@@ -1,7 +1,9 @@
+//controls the first button
 function rightFunction() {
   document.getElementById("imright").innerHTML = "I'm Right.";
 }
 
+//controls the second button
 function otherFunction() {
   document.getElementById("noMe").innerHTML = "No, I'm right!";
 }
---
+++ challenge_2/part_2/script2.js
@@ -0,0 +1,12 @@
+// when the mouse is hovering
+function newText() {
+  let text = document.querySelector("h1");
+  text.innerHTML = "Hey I told not to hover over me!";
+}
+
+// when the mouse is hovers out
+
+function normal() {
+  let norm = document.querySelector("h1");
+  norm.innerHTML = "you better not do it again";
+}
