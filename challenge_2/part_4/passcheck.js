7bdb3d2e5f09e4e5a02e05fd817b6fb337815b99
Athora190 <jademarte7@gmail.com>
2020-08-18 20:20:18 -0400

    linking css

--- challenge_2/part_4/passcheck.js
+++ challenge_2/part_4/passcheck.js
@@ -1,8 +1,9 @@
 let button = document.getElementById("button");
-button.addEventListener("click", check());
+button.addEventListener("click", check);
 //checks if the input is correct
 function check() {
-  let pass = document.getElementById("pass").submit();
+  let pass = document.getElementById("pass");
+
   if (pass.value == "12345678") {
     return alert("correct");
   } else {
